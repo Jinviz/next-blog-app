ac68d5c08a736c505372f8e1f794000ae49b797a
Ray <rlawlsdn9583@gmail.com>
2024-07-31 01:14:26 +0900

    etc: 들여쓰기 및 주석 추가

--- lib/post.tsx
+++ lib/post.tsx
@@ -12,31 +12,33 @@ console.log('postsDirectory', postsDirectory);
 export function getSortedPostsData() {
     // Get file names under /posts
     const fileNames = fs.readdirSync(postsDirectory);
-        // fileNames [`pre-redering.md`, `ssr-ssg.md`]
+    // fileNames [`pre-redering.md`, `ssr-ssg.md`]
     const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
+        // Remove ".md" from file name to get id
+        const id = fileName.replace(/\.md$/, '');
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    // 경로 /Users/jinwoo/project/next-blog-app/posts/pre-redering
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
+        // Read markdown file as string
+        const fullPath = path.join(postsDirectory, fileName);
+        // 경로 /Users/jinwoo/project/next-blog-app/posts/pre-redering
+        const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+        // Use gray-matter to parse the post metadata section 
+        const matterResult = matter(fileContents);
+        // 객체화
 
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data as {date: string; title: string},
-    };
-});
-  // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
+        // Combine the data with the id
+        return {
+            id,
+            ...(matterResult.data as {date: string; title: string})
+        };
+        // 반환문 중괄호 사용시 () 괄호 생략 가능
+    });
+    // Sort posts by date
+    return allPostsData.sort((a, b) => {
+        if (a.date < b.date) {
+        return 1;
+        } else {
+        return -1;
+        }
+    });
 }
